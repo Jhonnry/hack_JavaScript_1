81a79d08ac65d8733966974d0f589236137bc6a7
Yhon Altuve <147669752+Jhonnry@users.noreply.github.com>
2024-05-19 11:50:35 -0600

    Add files via upload

---
+++ hack_7.js
@@ -0,0 +1,21 @@
+/**
+ * ["foo","bar","baz","qux","echo"] 
+ * 
+ * modificar a mayúscula el primer caracter de cada item string del array
+ *  
+ * 
+ * output => ["Foo","Bar","Baz","Qux","Echo"]   
+ */
+let log = console.log;
+let arr = ["foo", "bar", "baz", "qux", "echo"];
+let len = arr.length;
+let result = [];
+
+for (let i = 0; i < len ; i++) {
+    result.push(arr[i][0].toUpperCase() + arr[i].substring(1))
+}
+
+log(result);
+
+//export result
+module.exports = result;
